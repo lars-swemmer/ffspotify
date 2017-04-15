35d916c013258cd4b25698f6709e97df17d2f654
Lars Swemmer <larsswemmer@gmail.com>
2017-04-15 15:58:33 +0200

    V1 front-end for index

--- resources/views/spotify/index.blade.php
+++ resources/views/spotify/index.blade.php
@@ -7,26 +7,89 @@
         <title>Spotify index</title>
         <!-- webpack broken -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="/css/spotify.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:500,700" rel="stylesheet">
     </head>
     <body>
-        <div class="container">
-            <div class="row main-header">
-                <div class="col-md-12">
-                    <div>
-                        <img src="http://www.purplepandamedia.com/wp-content/uploads/2015/03/spotify.jpg" height="40">
-                    </div>
+        
+        <div class="container-fluid hero-wrapper">
+            <div class="container header-wrapper">
+
+                <div class="text-center main-logo">
+                    <img src="images/main-logo-white.png">
+                </div> <!-- main-logo -->
+
+                <div class="text-center main-copy">
+                    {{-- <h1>Follow me on <img class=spotify-logo src="images/spotify.png"> Spotify</h1> --}}
+                    <h1>Follow me on <i class="fa fa-spotify" aria-hidden="true"></i> Spotify</h1>
+                    <h3>and be the first to hear my brand new music</h3>
+                    {{-- <button type="button" class="btn btn-lg btn-success">Connect with <i class="fa fa-spotify" aria-hidden="true"></i> Spotify</button> --}}
+                    <button type="button" class="btn btn-lg btn-success">Click here to connect &amp; follow</button>
                 </div>
-            </div>
-            <div class="row main-content">
-                <div class="col-md-6">
-                    <div class="artwork">
-                        <img src="https://u.scdn.co/images/pl/default/651ec36242608e43f6fd96792a7869194420247a">
-                    </div>
-                    <a class="btn btn-default" href="#" role="button">Follow Playlist</a>
+
+                <div class="main-phone text-center">
+                    <img src="images/phone.png">
                 </div>
-                <div class="col-md-6">Songs</div>
-            </div>
-        </div>
+
+            </div>  <!-- hero-wrapper -->
+        </div> <!-- hero-wrapper -->
+
+        <div class="container-fluid footer-wrapper">
+            <div class="container footer-content">
+                <div class="text-center latest-releases-copy">
+                    <img src="images/avatar.png">
+                    <h2>Latest Releases</h2>
+                </div> <!-- latest-releases-copy -->
+
+                <div class="latest-releases-covers">
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-xs-12 thumb">
+                        <a class="thumbnail" href="#">
+                            <img class="img-responsive" src="https://i.scdn.co/image/438b5e69ca0aab2c944e8b2cb18cc77c94c019e0" alt="">
+                        </a>
+                    </div>
+                    <div class="clearfix"></div>
+                </div> <!-- latest-releases-covers -->
+
+            </div> <!-- footer-content -->
+        </div> <!-- footer-wrapper -->
+
     </body>
 </html>
+
+<!-- rows tussen cols en containers -->
