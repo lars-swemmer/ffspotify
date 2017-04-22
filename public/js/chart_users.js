dcbf7a0fb23da237e93bd38e81127b7963374eb6
Lars Swemmer <larsswemmer@gmail.com>
2017-04-22 21:15:18 +0200

    Add dummy graph data to users

---
+++ public/js/chart_users.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+
+	var ctx = document.getElementById("graph");
+	var myChart = new Chart(ctx, {
+	    type: 'line',
+	    data: {
+	        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+	        datasets: [{
+	            label: '# of users',
+	            data: [12, 19, 3, 5, 2, 3, 6],
+	            backgroundColor: "rgba(75,192,192,0.4)",
+	            borderColor: "rgba(75,192,192,1)",
+	            borderWidth: 1
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            yAxes: [{
+	                ticks: {
+	                    beginAtZero:true
+	                }
+	            }]
+	        }
+	    }
+	});
+
+}
--- resources/views/fanbase/users.blade.php
+++ resources/views/fanbase/users.blade.php
@@ -7,7 +7,7 @@
             <h2 class="dashhead-title">Users</h2>
         </div>
 
-        {{-- <div class="btn-toolbar dashhead-toolbar">
+       {{--  <div class="btn-toolbar dashhead-toolbar">
             <div class="btn-toolbar-item input-with-icon">
                 <input type="text" value="01/01/15 - 01/08/15" class="form-control" data-provide="datepicker">
                 <span class="icon icon-calendar"></span>
@@ -33,6 +33,12 @@
     </div>
   </div>
 </div> --}}
+
+<canvas id="graph" style="width: 100%;"></canvas>
+
+<div class="hr-divider m-t-md m-b">
+        <h3 class="hr-divider-content hr-divider-heading">Users</h3>
+    </div>
 
 <div class="table-full">
   <div class="table-responsive">
@@ -88,5 +94,7 @@
     </li>
   </ul>
 </div> --}}
+
+<script src="{{ asset('js/chart_users.js') }}"></script>
 
 @endsection
