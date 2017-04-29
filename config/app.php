3a3d18ebe81e9b1262e27e2cb734d39844d06d15
Lars Swemmer <larsswemmer@gmail.com>
2017-04-29 14:25:49 +0200

    Change app name to lowercase

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Follow Me On Spotify',
+    'name' => 'Follow me on spotify',
 
     /*
     |--------------------------------------------------------------------------
