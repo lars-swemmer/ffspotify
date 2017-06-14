4a97e5d017d40c0eea3d82fbb32ffd56d781fae1
Lars Swemmer <larsswemmer@gmail.com>
2017-06-15 01:07:21 +0200

    Final pixel edits for r3hab page

--- resources/views/spotify/success.blade.php
+++ resources/views/spotify/success.blade.php
@@ -30,11 +30,47 @@
         &noscript=1"/>
         </noscript>
         <!-- End Facebook Pixel Code -->
+
+        <!-- Google Code for Remarketing Tag -->
+        <script type="text/javascript">
+        /* <![CDATA[ */
+        var google_conversion_id = 849670675;
+        var google_custom_params = window.google_tag_params;
+        var google_remarketing_only = true;
+        /* ]]> */
+        </script>
+        <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+        </script>
+        <noscript>
+        <div style="display:inline;">
+        <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/849670675/?guid=ON&amp;script=0"/>
+        </div>
+        </noscript>
     </head>
     <body>
         
           @if (session('completed_steps'))
+            <!-- fb conversion -->
             <script> fbq('track', 'SpotifyReleaseSubscription'); </script>
+
+            <!-- Google Code for Spotify follow R3HAB night Conversion Page -->
+            <script type="text/javascript">
+            /* <![CDATA[ */
+            var google_conversion_id = 849670675;
+            var google_conversion_language = "en";
+            var google_conversion_format = "3";
+            var google_conversion_color = "ffffff";
+            var google_conversion_label = "aB_vCPyJgHIQk-STlQM";
+            var google_remarketing_only = false;
+            /* ]]> */
+            </script>
+            <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+            </script>
+            <noscript>
+            <div style="display:inline;">
+            <img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/849670675/?label=aB_vCPyJgHIQk-STlQM&amp;guid=ON&amp;script=0"/>
+            </div>
+            </noscript>
           @endif
 
         <div class="container-fluid hero-wrapper" style="background-image:url('/images/{{ $artist->image_background }}');">
@@ -43,16 +79,6 @@
                 <div class="text-center main-logo">
                     <img src="/images/{{ $artist->logo }}">
                 </div> <!-- main-logo -->
-
-                {{-- <div class="text-center main-copy">
-                    <h1><i class="fa fa-check-circle-o" aria-hidden="true"></i> Thanks for following me!</h1>
-                    <h3>You're now the first to hear my brand new music</h3>
-                    <a target="_blank" href="https://open.spotify.com/user/{{ $playlist->user_id }}/playlist/{{ $playlist->playlist_id }}" class="btn btn-lg btn-success">Listen now</a>
-                </div>
-
-                <div class="main-phone text-center">
-                    <img src="/images/{{ $artist->image_playlist }}">
-                </div> --}}
 
                 <div class="text-center main-copy">
                     <h1>Thanks for following me!</h1>
@@ -100,6 +126,17 @@
 
             </div> <!-- footer-content -->
         </div> <!-- footer-wrapper -->
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-101078711-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
 
     </body>
 </html>
